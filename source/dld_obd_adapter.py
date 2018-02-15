033bf926b45af1eb2f70db5d4a6426cd5b6370b7
Victor Li <victorli8@icloud.com>
2018-02-14 20:50:18 -0800

    Adding async to OBD adapter

--- source/dld_obd_adapter.py
+++ source/dld_obd_adapter.py
@@ -15,29 +15,34 @@ import time
 # python OBD (from pyOBD-pi fork)
 import obd
 
+# establish a global command array
+commands = []
+
+# setting up the command array
+commands.append(obd.commands.ENGINE_LOAD)
+commands.append(obd.commands.RPM)
+commands.append(obd.commands.SPEED)
+commands.append(obd.commands.THROTTLE_POS)
+commands.append(obd.commands.RELATIVE_THROTTLE_POS)
+commands.append(obd.commands.RUN_TIME)
+commands.append(obd.commands.FUEL_LEVEL)
+commands.append(obd.commands.COOLANT_TEMP)
+commands.append(obd.commands.OIL_TEMP)
+commands.append(obd.commands.AMBIANT_AIR_TEMP)
+commands.append(obd.commands.BAROMETRIC_PRESSURE)
+
+# run in async mode?
+runAsync = True
+
 def obdSnapshot(obdConnection):
-    commands = []
     logTime = time.time()
 
     # snapshot time start (grabs computer time)
-    csvLine = "@@" + str(time.ctime()) + ","
-
-    # setting up the command array
-    commands.append(obd.commands.ENGINE_LOAD)
-    commands.append(obd.commands.RPM)
-    commands.append(obd.commands.SPEED)
-    commands.append(obd.commands.THROTTLE_POS)
-    commands.append(obd.commands.RELATIVE_THROTTLE_POS)
-    commands.append(obd.commands.RUN_TIME)
-    commands.append(obd.commands.FUEL_LEVEL)
-    commands.append(obd.commands.COOLANT_TEMP)
-    commands.append(obd.commands.OIL_TEMP)
-    commands.append(obd.commands.AMBIANT_AIR_TEMP)
-    commands.append(obd.commands.BAROMETRIC_PRESSURE)
+    csvLine = "@" + str(time.ctime()) + ","
 
     # execute the command array, saving results to csvLine with "," delimitation
     for i in xrange(0,len(commands)-1):
-        csvLine += str(obdConnection.query(commands[i]).value) + ","
+            csvLine += str(obdConnection.query(commands[i]).value) + ","
 
     csvLine += str(obdConnection.query(commands[len(commands)-1]).value) + "\n"
 
@@ -49,19 +54,29 @@ def obdSnapshot(obdConnection):
     # debug statement; outputs the time taken to complete the query
     print str(time.time()-logTime)
 
+# grabs all DTCs and outputs them to file
 def fetchDTC(obdConnection):
     dtcErrorFile = open(sys.argv[2] + "/dtcErrors",'a')
     dtcErrorFile.write(str(obdConnection.query(obd.commands.GET_DTC)))
     dtcErrorFile.close()
     print "Fetched DTC codes to file."
 
+# commands the vehicle to delete all of its stored DTCs
 def clearDTC(obdConnection):
     obdConnection.query(obd.commands.CLEAR_DTC)
     print "Cleared DTC codes!"
 
+# set the command array to watch mode, allowing for async non-blocking updates
+def obdAsync(obdConnection):
+    for i in xrange(0, len(commands)):
+        obdConnection.watch(commands[i])
+
 if __name__ == '__main__':
     ## Set up connection to the OBDLink MX
-    obdBluetoothConnection = obd.OBD(fast=True)
+    if runAsync:
+        obdBluetoothConnection = obd.Async(baudrate=115200,fast=True)
+    else:
+        obdBluetoothConnection = obd.OBD(baudrate=115200,fast=True)
 
     ## Logic based on command line call
     if(sys.argv[1] == "snapshot"):
@@ -70,3 +85,5 @@ if __name__ == '__main__':
         fetchDTC(obdBluetoothConnection)
     elif(sys.argv[1] == "clear"):
         clearDTC(obdBluetoothConnection)
+
+    obdBluetoothConnection.close()
