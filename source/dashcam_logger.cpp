dd5266641ba21471fb016b42ba0cce2597981bf6
Victor Li <victorli8@icloud.com>
2018-02-18 23:23:21 -0800

    Adding refactored files

---
+++ source/dashcam_logger.cpp
@@ -0,0 +1,72 @@
+/* ------------------------------------
+   Axolotl Dashcam Daemon
+   ------------------------------------
+   Background process managed by Axolotl;
+   logs camera data from dashcam and backup camera.
+*/
+
+#include "dcomh.hpp"
+
+#define DEBUG
+
+using namespace std;
+
+string loggingDirectory;
+bool loggingActive = true;
+
+/*
+  Toggles the data logging system off.
+*/
+void toggleHandler(int signumber, siginfo_t *siginfo, void *pointer) {
+  loggingActive = !loggingActive;
+}
+
+/*
+  Registers the toggle handler with SIGUSR1.
+*/
+void registerToggleHandler() {
+  static struct sigaction dsa;
+  memset(&dsa, 0, sizeof(dsa));
+  dsa.sa_sigaction = toggleHandler;
+  dsa.sa_flags = SA_SIGINFO;
+  sigaction(SIGUSR1, &dsa, NULL);
+}
+
+// record function that will record a 5-minute chunk of video from a stream over the wlan
+void record() {
+  printf("Recording...\n");
+  sleep(10);
+  printf("Recording complete. Saving to file.\n");
+}
+
+// loops the recording function
+void cameraLooper() {
+  clock_t timer1;
+  while(1) {
+    if(loggingActive) {
+      timer1 = clock();
+      if (axolotlFileSystem::getAvailableMemory("/Volumes/SD Transfer") > 2048) {
+        record();
+      }
+
+      #ifdef DEBUG
+      printf("Logged?\n");
+      printf("%f\n",(clock()-timer1)/(double)CLOCKS_PER_SEC);
+      printf("Sample Rate: %f\n",1/((clock()-timer1)/(double)CLOCKS_PER_SEC));
+      #endif
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  // Ensure that a logging directory has been provided and bind it
+  if (argc != 1) {
+    exit(1);
+  }
+  loggingDirectory = argv[0];
+
+  registerToggleHandler();
+  cameraLooper();
+
+  return 0;
+}
---
+++ source/data_logger.cpp
@@ -0,0 +1,107 @@
+/* ------------------------------------
+   Axolotl Data Logging Daemon
+   ------------------------------------
+   Background process managed by Axolotl;
+   logs all OBDII data with the help of a Python adapter.
+*/
+
+#include "dcomh.hpp"
+#include <python2.7/Python.h>
+
+#define DEBUG
+#define OBD_ADAPTER_PATH "/Gitdir/wombat/source/data_obd_adapter.py"
+
+using namespace std;
+
+string loggingDirectory;
+bool loggingActive = true;
+
+/*
+  Toggles the data logging system off.
+*/
+void toggleHandler(int signumber, siginfo_t *siginfo, void *pointer) {
+  loggingActive = !loggingActive;
+}
+
+/*
+  Registers the toggle handler with SIGUSR1.
+*/
+void registerToggleHandler() {
+  static struct sigaction dsa;
+  memset(&dsa, 0, sizeof(dsa));
+  dsa.sa_sigaction = toggleHandler;
+  dsa.sa_flags = SA_SIGINFO;
+  sigaction(SIGUSR1, &dsa, NULL);
+}
+
+/*
+  A loops that conducts all of the data logging.
+  Makes calls to the python adapter and then waits 100ms
+  in order to achieve ~10 samples per second.
+*/
+void loggingLooper() {
+  string builtCommand;
+  clock_t timer1;
+  while(1) {
+    timer1 = clock();
+    if (loggingActive) {
+      if(axolotlFileSystem::getAvailableMemory(loggingDirectory) > 200) {
+        string builtCommand = "python " + axolotlFileSystem::getHomeDir() + OBD_ADAPTER_PATH + " snapshot " + loggingDirectory;
+        system(builtCommand.c_str());
+      }
+
+      #ifdef DEBUG
+      printf("Logged?\n");
+      printf("%f\n",(clock()-timer1)/(double)CLOCKS_PER_SEC);
+      printf("Sample Rate: %f\n",1/((clock()-timer1)/(double)CLOCKS_PER_SEC));
+      #endif
+    }
+    usleep(1000);
+  }
+}
+
+void createLogfile() {
+  string createCommand;
+  if (axolotlFileSystem::getAvailableMemory(loggingDirectory) > 200) {
+    createCommand = "touch " + loggingDirectory + "/obd_log.csv";
+    system(createCommand.c_str());
+
+    #ifdef DEBUG
+    printf("We done got enough memory.\n");
+    printf("Logfile created.\n");
+    #endif
+  }
+  else {
+    #ifdef DEBUG
+    printf("Not enough free space. Waiting...");
+    #endif
+
+    while(axolotlFileSystem::getAvailableMemory(loggingDirectory) < 200) {
+      sleep(5);
+    }
+
+    createCommand = "touch " + loggingDirectory + "/obd_log.csv";
+    system(createCommand.c_str());
+
+    #ifdef DEBUG
+    printf("Logfile created after waiting.\n");
+    #endif
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  // Ensure that a logging directory has been provided and bind it
+  if (argc != 1) {
+    exit(1);
+  }
+  loggingDirectory = argv[0];
+
+  // Create the .csv where data will be logged
+  createLogfile();
+
+  // Start Data Logging
+  loggingLooper();
+
+  return 0;
+}
