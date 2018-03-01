65464fa8f2608c3b208123beb615327b26e791c6
Victor Li <victorli8@icloud.com>
2018-02-28 20:38:10 -0800

    Wmanager fixes

--- source/wmanager/set_client.sh
+++ source/wmanager/set_client.sh
@@ -1,6 +1,7 @@
 sudo killall hostapd
 sudo killall wpa_supplicant
 
+sudo cp orig/bcmdhd.conf /etc/modprobe.d/bcmdhd.conf
 sudo cp orig/sysctl.conf /etc/sysctl.conf
 sudo cp orig/interfaces /etc/network/interfaces
 sudo cp orig/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
@@ -11,3 +12,4 @@ sudo nmcli radio wifi on
 
 sudo service NetworkManager restart
 sudo service wpa_supplicant start
+
