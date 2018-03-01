b00f3f9d7b4148de75b5c5ab22cab8ad62180416
Victor Li <victorli8@icloud.com>
2018-02-28 16:35:05 -0800

    Adding wifi manager code

---
+++ source/wmanager/set_client.sh
@@ -0,0 +1,11 @@
+sudo killall hostapd
+sudo service wpa_supplicant stop
+
+sudo cp ap/bcmdhd.conf /etc/modprobe.d/bcmdhd.conf
+sudo cp ap/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ap/sysctl.conf /etc/sysctl.conf
+sudo cp ap/interfaces /etc/network/interfaces
+sudo cp ap/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
+
+sudo service NetworKManager restart
+sudo service wpa_supplicant restart
