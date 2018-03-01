6957912d70d6b5ac97d2889f0514b87c25d4476e
Victor Li <victorli8@icloud.com>
2018-02-28 17:22:55 -0800

    Adding fully working working AP wifi toggle

--- source/wmanager/set_ap.sh
+++ source/wmanager/set_ap.sh
@@ -1,11 +1,15 @@
 sudo killall hostapd
 sudo killall wpa_supplicant 
 
-sudo cp ap/bcmdhd.conf /etc/modprobe.d/bcmdhd.conf
 sudo cp ap/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ap/dnsmasq.conf /etc/dnsmasq.conf
 sudo cp ap/sysctl.conf /etc/sysctl.conf
 sudo cp ap/interfaces /etc/network/interfaces
 sudo cp ap/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
+
+sudo nmcli radio wifi off
+sudo su -c "echo 2 > sudo /sys/module/bcmdhd/parameters/op_mode"
+sudo nmcli radio wifi on
 
 sudo service NetworkManager restart
 sudo hostapd -B /etc/hostapd/hostapd.conf
