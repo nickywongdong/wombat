3a7f4785223bbf9dd4cc9c5f41bc634616b293e3
Victor Li <victorli8@icloud.com>
2018-03-02 15:01:56 -0800

    Force yes

--- source/installdep.sh
+++ source/installdep.sh
@@ -2,36 +2,35 @@
 
 sudo apt-get update
 
-sudo apt-get install build-essential cmake
-sudo apt-get install build-essential zlib1g-dev
-sudo apt-get install build-essential libpng12-dev
-sudo apt-get install build-essential libgtk2.0-dev
-sudo apt-get install build-essential librsvg2-bin
-sudo apt-get install build-essential g++
-sudo apt-get install build-essential build-essential gpsd
-sudo apt-get install build-essential gpsd-clients
-sudo apt-get install build-essential libgps-dev
-sudo apt-get install build-essential libdbus-glib-1-dev
-sudo apt-get install build-essential libimlib2-dev
-sudo apt-get install build-essential espeak
+sudo apt-get install build-essential cmake -y
+sudo apt-get install build-essential zlib1g-dev -y
+sudo apt-get install build-essential libpng12-dev -y
+sudo apt-get install build-essential libgtk2.0-dev -y
+sudo apt-get install build-essential librsvg2-bin -y
+sudo apt-get install build-essential g++ -y
+sudo apt-get install build-essential build-essential gpsd -y
+sudo apt-get install build-essential gpsd-clients -y
+sudo apt-get install build-essential libgps-dev -y
+sudo apt-get install build-essential libdbus-glib-1-dev -y
+sudo apt-get install build-essential libimlib2-dev -y
+sudo apt-get install build-essential espeak -y
 
-sudo apt-get install build-essential python
-sudo apt-get install build-essential python-pip
+sudo apt-get install build-essential python -y
+sudo apt-get install build-essential python-pip -y
 sudo pip install --upgrade pip
 sudo pip install obd
 
-sudo apt-get install build-essential libboost-all-dev
-sudo apt-get install build-essential libcrypto++
+sudo apt-get install build-essential libboost-all-dev -y
+sudo apt-get install build-essential libcrypto++ -y
 
-sudo apt-get install build-essential libbluetooth-dev
-
-sudo apt-get install espeak
+sudo apt-get install build-essential libbluetooth-dev -y
+sudo apt-get install espeak -y
 
 sudo acm/installCDCACM.sh
 
-sudo apt-get install build-essential hostapd
-sudo apt-get install build-essential dnsmasq
+sudo apt-get install build-essential hostapd -y
+sudo apt-get install build-essential dnsmasq -y
 
-sudo apt-get install build-essential pulseaudio
+sudo apt-get install build-essential pulseaudio -y
 
 sudo reboot
