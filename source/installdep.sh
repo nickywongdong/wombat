0d3e4241cc93f942d63baa372615103c8f8cd285
Victor Li <victorli8@icloud.com>
2018-03-02 02:16:20 -0800

    Modifying dependency install script

--- source/installdep.sh
+++ source/installdep.sh
@@ -17,8 +17,8 @@ sudo apt-get install build-essential espeak
 
 sudo apt-get install build-essential python
 sudo apt-get install build-essential python-pip
-pip install --upgrade pip
-pip install obd
+sudo pip install --upgrade pip
+sudo pip install obd
 
 sudo apt-get install build-essential libboost-all-dev
 sudo apt-get install build-essential libcrypto++
