4a93f9d9feb9e1eb38f56441a9840a183f57d8a4
wongnich <wongnich@gmail.com>
2018-03-03 13:08:48 -0800

    updating path for portability

--- source/media_player/run.sh
+++ source/media_player/run.sh
@@ -3,6 +3,6 @@
 #This script will open VLC media player with the directory specified.
 #Still need to test to see if it will open the playlist by default or if we need to enable it
 
-vlc --playlist-autostart --loop --playlist-tree ~/Desktop/Music_Test
+vlc --playlist-autostart --loop --playlist-tree Music
 
 
