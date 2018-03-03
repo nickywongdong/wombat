d13e6f2fa590722b604a1acda7344ec34f2cd166
wongnich <wongnich@gmail.com>
2018-03-03 12:47:11 -0800

    fixing path of bash script

--- source/media_player/run.sh
+++ source/media_player/run.sh
@@ -3,6 +3,6 @@
 #This script will open VLC media player with the directory specified.
 #Still need to test to see if it will open the playlist by default or if we need to enable it
 
-vlc --playlist-autostart --loop --playlist-tree home/nvidia/Desktop
+vlc --playlist-autostart --loop --playlist-tree ~/Desktop/Music_Test
 
 
