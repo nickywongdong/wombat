c8e3432b4f3238565bf94562bdd0a4faad51658f
wongnich <wongnich@gmail.com>
2018-03-13 13:52:50 -0700

    uploading initial stages for GPIO switch toggle. Still need to test on the Jetson

---
+++ source/sleep_mode/JetsonGPIO.h
@@ -0,0 +1,90 @@
+/*
+ * jetsonGPIO.h
+ *
+ * Copyright (c) 2015 JetsonHacks
+ * www.jetsonhacks.com
+ *
+ * Based on Software by RidgeRun
+ * Originally from:
+ * https://developer.ridgerun.com/wiki/index.php/Gpio-int-test.c
+ * Copyright (c) 2011, RidgeRun
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the RidgeRun.
+ * 4. Neither the name of the RidgeRun nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RIDGERUN ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL RIDGERUN BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef JETSONGPIO_H_
+#define JETSONGPIO_H_
+
+ /****************************************************************
+ * Constants
+ ****************************************************************/
+
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+#define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
+#define MAX_BUF 64
+
+typedef unsigned int jetsonGPIO ;
+typedef unsigned int pinDirection ;
+typedef unsigned int pinValue ;
+
+enum pinDirections {
+	inputPin  = 0,
+	outputPin = 1
+} ;
+
+enum pinValues {
+    low = 0,
+    high = 1,
+    off = 0,  // synonym for things like lights
+    on = 1
+}  ;
+
+
+//Adding Jetson TX2 GPIO for some pins taken from JetsonHacks Pinmux
+
+enum jetsonTX2GPIONumber {
+       gpio296 = 296,      // J21 - Pin 16 - Bidir   - Unused
+       gpio297 = 297,      // J21 - Pin 32 - Bidir   - Unused
+       gpio398 = 398,      // J21 - Pin 29 - unknown - Audio Reset
+       gpio388 = 388,      // J21 - Pin 31 - unknown - Motion Interrupt
+       gpio389 = 389,      // J21 - Pin 33 - unknown - AP Wake Bt GPIO
+} ;
+
+
+int gpioExport ( jetsonGPIO gpio ) ;
+int gpioUnexport ( jetsonGPIO gpio ) ;
+int gpioSetDirection ( jetsonGPIO, pinDirection out_flag ) ;
+int gpioSetValue ( jetsonGPIO gpio, pinValue value ) ;
+int gpioGetValue ( jetsonGPIO gpio, unsigned int *value ) ;
+int gpioSetEdge ( jetsonGPIO gpio, char *edge ) ;
+int gpioOpen ( jetsonGPIO gpio ) ;
+int gpioClose ( int fileDescriptor ) ;
+int gpioActiveLow ( jetsonGPIO gpio, unsigned int value ) ;
+
+
+
+#endif // JETSONGPIO_H_
---
+++ source/sleep_mode/switch_toggle.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+#include <sys/time.h>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include "JetsonGPIO.h"
+
+
+//gpio pins for output, and input:
+jetsonTX2GPIONumber OUT = gpio296;
+jetsonTX2GPIONumber IN1 = gpio297;
+//later use
+//jetsonTX2GPIONumber IN2;
+
+int getkey();
+
+
+int main(int argc, char *argv[]){
+
+    std::cout << "Toggle Switch Testing" << std::endl;
+
+    // Make the button and led available in user space
+    gpioExport(OUT) ;
+    gpioExport(IN1) ;
+    //original directions:
+    //gpioSetDirection(pushButton,inputPin) ;
+    //gpioSetDirection(redLED,outputPin) ;
+    gpioSetDirection(OUT, outputPin);
+    gpioSetDirection(IN1, inputPin);
+
+    // Reverse the button wiring; this is for when the button is wired
+    // with a pull up resistor
+    // gpioActiveLow(pushButton, true);
+
+
+    /*// Flash the LED 5 times
+    for(int i=0; i<5; i++){
+        cout << "Setting the LED on" << endl;
+        gpioSetValue(redLED, on);
+        usleep(200000);         // on for 200ms
+        cout << "Setting the LED off" << endl;
+        gpioSetValue(redLED, off);
+        usleep(200000);         // off for 200ms
+    }*/
+
+    //Instead of waiting for the LED to be pressed, we should wait for the
+    //Second input pin to receive a change in value later.
+    // Wait for the push button to be pressed
+    std::cout << "Please press the button! ESC key quits the program" << std::endl;
+
+    unsigned int value = low;
+    int outputValue = low ;
+    // set output value to low
+    gpioSetValue(OUT,low) ;
+    while(getkey() != 27) {
+        gpioGetValue(IN1, &value) ;
+        // Useful for debugging
+        // cout << "Input Value: " << value << endl;
+        if (value==high && outputValue != high) {
+            // button is pressed ; turn the LED on
+            outputValue = high ;
+            gpioSetValue(OUT,on) ;
+        } else {
+            // button is *not* pressed ; turn the LED off
+            if (outputValue != low) {
+                outputValue = low ;
+                gpioSetValue(OUT,off) ;
+            }
+
+        }
+        usleep(1000); // sleep for a millisecond
+    }
+
+    std::cout << "Finished" << std::endl;
+    gpioUnexport(OUT);      // unexport the output pin
+    gpioUnexport(IN1);      // unexport the input pin
+    //gpioUnexport(IN2);
+    return 0;
+}
+
+//seems to be a function to grab character from stdin
+int getkey() {
+    int character;
+    struct termios orig_term_attr;
+    struct termios new_term_attr;
+
+    /* set the terminal to raw mode */
+    tcgetattr(fileno(stdin), &orig_term_attr);
+    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+    new_term_attr.c_lflag &= ~(ECHO|ICANON);
+    new_term_attr.c_cc[VTIME] = 0;
+    new_term_attr.c_cc[VMIN] = 0;
+    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+
+    /* read a character from the stdin stream without blocking */
+    /*   returns EOF (-1) if no character is available */
+    character = fgetc(stdin);
+
+    /* restore the original terminal attributes */
+    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+
+    return character;
+}
