080a09a26538dc65fb38029a43a486a238c5f7c6
Victor Li <victorli8@icloud.com>
2018-03-13 22:20:50 -0700

    Record process now properly recognizes commands

--- source/record.cpp
+++ source/record.cpp
@@ -21,10 +21,10 @@ int main(int argc, char *argv[]) {
   int cameraPort = atoi(argv[1]);
   string runType = argv[2], loggingDirectory = argv[3], sysCmd;
 
-  if(runType = "r") {
+  if(runType == "r") {
     string sysCmd = "gst-launch-1.0 -v udpsrc port=" + to_string(cameraPort) + " ! gdpdepay ! rtph264depay ! avdec_h264 ! autovideosink sync=false";
   }
-  else if (runType = "w") {
+  else if (runType == "w") {
     string sysCmd = "gst-launch-1.0 -v udpsrc port=" + to_string(cameraPort) + " ! gdpdepay ! rtph264depay ! avdec_h264 ! autovideosink sync=false";
   }
 
