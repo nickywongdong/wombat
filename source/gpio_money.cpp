bba95495417b353aef28f9a6854e320151a9d7d2
Supbrah <wongnich@gmail.com>
2018-03-15 22:43:11 +0000

    reverse detection wwwwweeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ source/gpio_money.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<signal.h>
+#include<unistd.h>
+
+
+int main(){
+using namespace std;
+ifstream f;
+int i;
+bool streaming = false;
+
+while(true){
+	f.open("/sys/class/gpio/gpio298/value");
+	f >> i;
+	if(i == 1 && not(streaming)){
+		kill(getppid(),SIGBUS);
+		//cout << i <<"\n";
+		streaming = true;
+		sleep(1);
+	}
+	else if(i == 0 && streaming){
+		kill(getppid(),SIGBUS);
+		//cout << i << "\n";
+		streaming = false;
+		sleep(1);
+	}
+	f.close();
+}
+
+}
+
+
+
