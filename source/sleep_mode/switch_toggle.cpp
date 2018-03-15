17ef09c60beb9d570da7d7beb8aef72a76cf5f94
wongnich <wongnich@gmail.com>
2018-03-15 16:53:37 -0700

    moving the switch toggle back to original

--- source/sleep_mode/switch_toggle.cpp
+++ source/sleep_mode/switch_toggle.cpp
@@ -8,10 +8,7 @@
 #include <string>
 #include <unistd.h>
 #include <csignal>
-#include <sys/time.h>
-#include <poll.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <linux/gpio.h>
 #include "JetsonGPIO.h"
 
 
@@ -26,11 +23,6 @@ int getkey();
 
 int main(int argc, char *argv[]){
 
-	char str[256];
-   	struct timeval tv;
-   	struct pollfd pfd;
-   	char buf[8];
-
     std::cout << "Toggle Switch Testing" << std::endl;
 
     // Make the button and led available in user space
@@ -43,16 +35,6 @@ int main(int argc, char *argv[]){
     //gpioSetDirection(redLED,outputPin) ;
     //gpioSetDirection(OUT, outputPin);
     gpioSetDirection(IN1, inputPin);
-
-    //setup pin as interrupt
-    sprintf(str, "/sys/class/gpio/gpio%d/value", IN1);
-
-   	/*if ((fd = open(str, O_RDONLY)) < 0) {
-      fprintf(stderr, "Failed, gpio %d not exported.\n", IN1);
-      exit(1);
-  	}*/
-
-
 
     // Reverse the button wiring; this is for when the button is wired
     // with a pull up resistor
@@ -69,42 +51,12 @@ int main(int argc, char *argv[]){
         gpioGetValue(IN1, &value) ;
         // Useful for debugging
         // cout << "Input Value: " << value << endl;
-        if ( value==high ) {
-        	std::cout << "Pin is High" << std::endl;
-        	system("sleep 1");
+        if ( value==low ) {
+
+        	std::cout << "Pin is Low " << std::endl;
+        	system("sleep 1 ");
             //Switch is toggled on, tell Jetson to sleep:
-            //system("echo mem > /sys/power/state");
-
-            //if( value == low ) {
-				//std::cout << "Pin is Low" << std::endl;
-
-        	//allow for interrupt on pin to wake system:
-
-				pfd.fd = value;
-
-				pfd.events = POLLPRI;
-
-				std::cout << "consuming priot interrupt" << std::endl;
-
-   				lseek(value, 0, SEEK_SET);    /* consume any prior interrupt */
-
-				std::cout << "after lseek: " << std::endl;
-
-				read(value, buf, sizeof buf);
-
-				std::cout << "waiting for interrupt: " << std::endl;
-   				poll(&pfd, 1, -1);         /* wait for interrupt */
-   				std::cout << "interrupt has ocurred " << std::endl;
-
-   				system("echo mem > /sys/power/state");
-
-   				lseek(value, 0, SEEK_SET);    /* consume interrupt */
-				read(value, buf, sizeof buf);
-
-			//}
-
-
-
+            system("echo mem > /sys/power/state")
         }
     }
 
