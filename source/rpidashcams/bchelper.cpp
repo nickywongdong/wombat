1af3effdaf3a7e9ea65cb344b110a9b82ece0dd5
Victor Li <victorli8@icloud.com>
2018-03-15 17:05:14 -0700

    Finalized backup camera trigger

--- source/rpidashcams/bchelper.cpp
+++ source/rpidashcams/bchelper.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 int main() {
-  sleep(10);
+  sleep(5);
   system("raspivid -t 999999 -w 1080 -h 720 -fps 30 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink host=192.168.8.1 port=9003 sync=false async=false");
   return 0;
 }
