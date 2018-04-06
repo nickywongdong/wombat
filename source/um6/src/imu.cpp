248d882a9e08abac873a6f13353967ca3d4ba93a
Victor Li <victorli8@icloud.com>
2018-04-05 17:47:10 -0700

    Using to_string

--- source/um6/src/imu.cpp
+++ source/um6/src/imu.cpp
@@ -295,7 +295,7 @@ int main(int argc, char **argv) {
               std::cout<<"Yaw Z = "<<std::fixed<<IMU_EULER_Z*360/6.28318530718<<std::endl<<std::endl;
 
               if(ahrs_csv.is_open()) {
-                ahrs_csv.write(ftoa(IMU_EULER_X*360/6.28318530718)+","+ftoa(IMU_EULER_Y*360/6.28318530718)+","+ftoa(IMU_EULER_Z*360/6.28318530718));
+                ahrs_csv.write(std::to_string(IMU_EULER_X*360/6.28318530718)+","+std::to_string(IMU_EULER_Y*360/6.28318530718)+","+std::to_string(IMU_EULER_Z*360/6.28318530718));
               }
 
               t = 0;
