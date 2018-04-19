aec891235d5f99488ee660d45fa8ee8dca4938cc
Victor Li <victorli8@icloud.com>
2018-04-18 17:38:33 -0700

    Fixing bad update-rc.d

--- source/rpidashcams/installrp1.sh
+++ source/rpidashcams/installrp1.sh
@@ -3,5 +3,6 @@ make
 sudo cp front_cam_autostart /etc/init.d/front_cam_autostart
 sleep 5s
 sudo chmod +x /etc/init.d/front_cam_autostart
+chdir /etc/init.d
 sudo update-rc.d front_cam_autostart defaults
 sudo reboot
--- source/rpidashcams/installrp2.sh
+++ source/rpidashcams/installrp2.sh
@@ -3,5 +3,6 @@ make
 sudo cp rear_cam_autostart /etc/init.d/rear_cam_autostart
 sleep 5s
 sudo chmod +x /etc/init.d/rear_cam_autostart
+chdir /etc/init.d
 sudo update-rc.d rear_cam_autostart defaults
 sudo reboot
