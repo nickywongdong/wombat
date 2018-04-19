52a837fcb2a812f5f7cf81d8476dfc6210705ad2
Victor Li <victorli8@icloud.com>
2018-04-18 23:08:43 -0700

    Adding comments

--- source/rpidashcams/installrp1.sh
+++ source/rpidashcams/installrp1.sh
@@ -6,7 +6,7 @@ sleep 5s
 # sudo chmod +x /etc/init.d/front_cam_autostart
 # chdir /etc/init.d
 # sudo update-rc.d front_cam_autostart defaults
-sudo cp -n /etc/rc.local rc.local
-sudo cp rc.local /etc/rc.local
-sudo echo "sudo /home/pi/wombat/source/rpidashcams/rpi1dcd &" >> /etc/rc.local
+sudo cp -n /etc/rc.local rc.local       # copy the original rc.local file here, but don't override it!
+sudo cp rc.local /etc/rc.local          # copy the master copy of the rc.local file back into etc
+sudo echo "sudo /home/pi/wombat/source/rpidashcams/rpi1dcd &" >> /etc/rc.local    # write our startup command into it
 sudo reboot
