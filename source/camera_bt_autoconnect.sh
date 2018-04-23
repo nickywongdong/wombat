6645305c3b3cff76eb456127ac1758aef9aad431
Victor Li <victorli8@icloud.com>
2018-04-23 13:20:49 -0700

    Fixing Jetson auto-connect bluetooth script

--- source/camera_bt_autoconnect.sh
+++ source/camera_bt_autoconnect.sh
@@ -1,12 +1,31 @@
-#! /bin/bash
+#!/bin/bash
+front=""
+rear=""
+pos=0
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	echo "Found BT ADDR: $line"
-	bt-device --disconnect=$line
-	bt-device --connect=$line
+
+	if [[ "$front" == "" ]]
+	then
+		front=$line
+	elif  [[ "$rear" == "" ]]
+	then
+		rear=$line
+	fi
+	pos+=1
+	# bt-device --disconnect=$line
+	# bt-device --connect=$line
 done < "bluetooth_addresses"
 
-echo $0
-echo $1
+echo $front
+echo $rear
 
+pactl load-module module-bluetooth-discover
+
+bluetoothctl << EOF
+connect $front
+connect $rear
+EOF
 #sudo hidd --connect $0 > /dev/null 2>&1
 #sudo hidd --connect $1 > /dev/null 2>&1
