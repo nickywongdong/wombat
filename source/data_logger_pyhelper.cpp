fb9c2bbd718fc6e413dfd08b7f3393eb54bc8058
Victor Li <victorli8@icloud.com>
2018-04-25 10:20:55 -0700

    Reform filepaths to absolute

--- source/data_logger_pyhelper.cpp
+++ source/data_logger_pyhelper.cpp
@@ -1,13 +1,13 @@
 /* ------------------------------------
    Axolotl Data Logger Python Helper
    ------------------------------------
-   Helper process that runs the OBD logging script separately from the data logging daemon process.
+   Execs the data_obd_logger.
 */
 
 #include "dcomh.hpp"
 
 #ifdef __linux__
-#define OBD_ADAPTER_PATH "/wombat/source/data_obd_logger.py"
+#define OBD_ADAPTER_PATH "/home/nvidia/wombat/source/data_obd_logger.py"
 #else
 #define OBD_ADAPTER_PATH "/Gitdir/wombat/source/data_obd_logger.py"
 #endif
