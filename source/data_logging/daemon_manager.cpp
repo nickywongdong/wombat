785cd74c75a6684880b03f2712607f416464393b
Victor Li <victorli8@icloud.com>
2018-04-27 13:56:15 -0700

    Casting getenv into string

--- source/data_logging/daemon_manager.cpp
+++ source/data_logging/daemon_manager.cpp
@@ -38,7 +38,7 @@ string logging_directory, run_directory, log_volume;
 bool checkPasswordCorrect(string password) {
   string truekey;
   ifstream truekeyf;
-  string hash_file_path = getenv("HOME") + "/wombat/source/data_logging/hashkey"; //normally run_directory + "/hashkey"
+  string hash_file_path = (char *)getenv("HOME") + "/wombat/source/data_logging/hashkey"; //normally run_directory + "/hashkey"
   truekeyf.open(hash_file_path);
   if(truekeyf.is_open()) {
     getline(truekeyf,truekey);
@@ -163,7 +163,7 @@ void deleteData(string password) {
   // Get true passkey hash from file
   ifstream hash_file;
   string truekeyHash = NULL;
-  string hash_file_path = getenv("HOME") + "/wombat/source/data_logging/hashkey"; //normally run_directory + "/hashkey"
+  string hash_file_path = (char *)getenv("HOME") + "/wombat/source/data_logging/hashkey"; //normally run_directory + "/hashkey"
   hash_file.open(hash_file_path);
   if (hash_file.is_open()) {
     getline(hash_file,truekeyHash);
@@ -187,7 +187,7 @@ bool changePassword(string checkPassword, string newPassword) {
   if(checkPasswordCorrect(checkPassword)) {
     ofstream hash_file;
     string newhash = axolotlFileSystem::hash(newPassword);
-    string hash_file_path = getenv("HOME") + "/wombat/source/data_logging/hashkey";    // normally run_directory + "/hashkey"
+    string hash_file_path = (char *)getenv("HOME") + "/wombat/source/data_logging/hashkey";    // normally run_directory + "/hashkey"
     hash_file.open(hash_file_path, std::ofstream::trunc);
     if(hash_file.is_open()) {
       hash_file.write((const char *)newhash.c_str(),(long)newhash.length());
@@ -204,7 +204,7 @@ bool changePassword(string checkPassword, string newPassword) {
 void resetPassword() {
   ofstream hash_file;
   string newhash = axolotlFileSystem::hash("orangemonkeyeagle");
-  string hash_file_path = getenv("HOME") + "/wombat/source/data_logging/hashkey"; // normally run_directory + "/hashkey"
+  string hash_file_path = (char *)getenv("HOME") + "/wombat/source/data_logging/hashkey"; // normally run_directory + "/hashkey"
   hash_file.open(hash_file_path, std::ofstream::trunc);
   if(hash_file.is_open()) {
     hash_file.write((const char *)newhash.c_str(),(long)newhash.length());
