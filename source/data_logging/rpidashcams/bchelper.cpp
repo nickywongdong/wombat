1095b48a37aa0bc9d7c3e660767d213cc804a8cf
Victor Li <victorli8@icloud.com>
2018-05-01 00:47:22 -0700

    Resyncing rear camera backup stream

--- source/data_logging/rpidashcams/bchelper.cpp
+++ source/data_logging/rpidashcams/bchelper.cpp
@@ -4,6 +4,6 @@
 
 int main() {
   sleep(5);
-  system("raspivid -t 0 -w 1920 -h 1080 -fps 30 -b 1000000 -vf -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink host=192.168.8.1 port=9003 sync=false");
+  system("raspivid -t 0 -w 1920 -h 1080 -fps 30 -b 1000000 -vf -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink host=192.168.8.1 port=9003");
   return 0;
 }
