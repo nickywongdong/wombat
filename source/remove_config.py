6b64af8f5ed3284a8fcc2d0ad0fd837fa1273ee5
Victor Li <victorli8@icloud.com>
2018-05-03 11:46:15 -0700

    Revising gqrx remove config

--- source/remove_config.py
+++ source/remove_config.py
@@ -3,10 +3,12 @@ import sys
 
 config_file = open("/home/nvidia/.config/gqrx/default.conf",'r')
 file_lines = config_file.readlines()
-file_lines = file_lines[3:]
 config_file.close()
+
+# file_lines = file_lines[3:]
 
 config_file = open("/home/nvidia/.config/gqrx/default.conf",'w')
 for line in file_lines:
-    config_file.write(line)
+    if not ("crash" in line):
+        config_file.write(line)
 config_file.close()
