6f747725aca582b26249302e96b724291a0061b7
Victor Li <victorli8@icloud.com>
2018-05-02 17:02:23 -0700

    Fixing config deleter

--- source/remove_config.py
+++ source/remove_config.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-config_file.open("/home/nvidia/.config/gqrx/default.conf",'r')
+config_file = open("/home/nvidia/.config/gqrx/default.conf",'r')
 file_lines = config_file.readlines()
 file_lines = file_lines[3:]
 config_file.close()
 
-config_file.open("/home/nvidia/.config/gqrx/default.conf",'w')
+config_file = open("/home/nvidia/.config/gqrx/default.conf",'w')
 for line in file_lines:
     config_file.write(line)
 config_file.close()
